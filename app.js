393fa7ad6f678d71c40d69af9450ac555d69923b
erica-ym <ericaym334@gmail.com>
2018-05-15 22:03:51 -0400

    streaming and export to JSON working

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 require('dotenv').config()
-var Twitter = require('twitter');
+var Twitter = require('twitter'); //the github readme: https://github.com/desmondmorris/node-twitter
 var fs = require('file-system');
 
 //set these as environment variables when you download
@@ -20,44 +20,43 @@ client.get('statuses/user_timeline', params, function(error, tweets, response) {
 });
 */
 
+//gets a JSON with the search query specified
 client.get('https://api.twitter.com/1.1/search/tweets.json?q=partido&l=es&src=typd', params, function(error, tweets, response) {
   if (!error) {
-    console.log(tweets);
-    var info = JSON.stringify(tweets, null, 4);
-    fs.writeFile('new.txt', info, 'utf8', function(err,data) {
-    if(err) {
-        console.log(err);
+    fs.writeFile('new.json', JSON.stringify(tweets, null, 4), 'utf8');
     } else {
-        fs.writeFile('new.json', info, 'utf8');
-        //fs.unlink('new.txt');
+      console.log(err);
     }
   });
-  }
+
+var streamingData = []
+
+//streaming API
+var stream = client.stream('statuses/filter', {track: 'airbnb'});
+  stream.on('data', function(event) {
+    // line below logs the entire event data
+    //console.log(event);
+    // line below logs just the tweet text
+    console.log(event && event.text);
+    var add = event;
+    streamingData.push(add);
+    fs.writeFile('test.json', JSON.stringify(streamingData, null, 4), 'utf8');
+    });
+
+stream.on('error', function(error) {
+    throw error;
 });
 
-//fs.writeFile('test.json', JSON.stringify({ a:1, b:2, c:3 }, null, 4));
+//from that JSON, get language and location
+//language: "iso_language_code" or "lang"
+//"location", "geo" or "time_zone" doesn't always exist
 
-
-/*
-var info = JSON.stringify(tweets);
-    fs.writeFile('new.txt', info, 'utf8', function(err,data) {
-    if(err) {
-        console.log(err);
-    }
-  });
-
-var update = JSON.parse('new.txt');
-fs.writeFile('newUPDATE.json', update, 'utf8', function(err,data) {
-    if(err) {
-        console.log(err);
-    }
-  });
-*/
-
-
-
-
-
+//Get your favourites
+// client.get('favorites/list', function(error, tweets, response) {
+//     if(error) throw error;
+//     console.log(tweets);  // The favorites.
+//     //console.log(response);  // Raw response object.
+// });
 
 
 // replace up to search with https://api.twitter.com/1.1/search/tweets.json
